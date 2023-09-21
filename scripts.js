a364c1edf79099f35a2a6832665cf24d6511d997
Andre <andre.huang2010@gmail.com>
2023-09-21 19:33:43 -0300

    Add buttons and logs result in console

--- scripts.js
+++ scripts.js
@@ -28,33 +28,40 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log(playRound(button.textContent, getComputerChoice()));
+  });
+});
 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection;
-        do {
-            playerSelection = prompt("Rock, paper or scissors?");
-        } while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissors");
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result.includes("Win")) {
-            playerScore += 1;
-        } else if (result.includes("Lose")) {
-            computerScore += 1;
-        }
-        console.log(`Score: Player = ${playerScore} Computer = ${computerScore}`)
-    }
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    if (playerScore > computerScore) {
-        return console.log("Player wins the best of 5!");
-    } else if (playerScore < computerScore) {
-        return console.log("Computer wins the best of 5!");
-    } else {
-        return console.log("The game ended in a draw!");
-    }
-}
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection;
+//         do {
+//             playerSelection = prompt("Rock, paper or scissors?");
+//         } while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissors");
+//         let computerSelection = getComputerChoice();
+//         let result = playRound(playerSelection, computerSelection);
+//         console.log(result);
+//         if (result.includes("Win")) {
+//             playerScore += 1;
+//         } else if (result.includes("Lose")) {
+//             computerScore += 1;
+//         }
+//         console.log(`Score: Player = ${playerScore} Computer = ${computerScore}`)
+//     }
 
-game();
+//     if (playerScore > computerScore) {
+//         return console.log("Player wins the best of 5!");
+//     } else if (playerScore < computerScore) {
+//         return console.log("Computer wins the best of 5!");
+//     } else {
+//         return console.log("The game ended in a draw!");
+//     }
+// }
+
+// game();
